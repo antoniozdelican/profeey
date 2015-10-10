340a5428d48f045c1b18c7ac6ee410f471821877
antoniozdelican <antonio.zdelican@gmail.com>
2015-10-10 22:48:41 +0100

    fixed header and added full_name for sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
+
+  validates :full_name, presence: true
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
