e0778a398497bd2e71001de24a6b90ffe756aded
antoniozdelican <antonio.zdelican@gmail.com>
2015-10-13 15:27:03 +0100

    finished js with typeahead for professions

---
+++ app/assets/javascripts/typeahead.js
@@ -0,0 +1,62 @@
+$(function() {
+  var professions = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    
+    // get professions json from database
+    // WARNING!! consult the storage limit!!
+    prefetch: 'professions',
+    
+    // receives objects in form {"id":1,"name":"Abbot","created_at":"2015-10-11T19:01:55.000Z","updated_at":"2015-10-11T19:01:55.000Z"}
+    sorter : function(a, b) {
+      var aName = a.name;
+      var bName = b.name;
+            
+      // get input text
+      var inputString = $('input.typeahead.tt-input').val();
+      
+      // move exact matches to top
+      if (inputString == aName) { return -1; }
+      if (inputString == bName) { return 1; }
+
+      // close match without case matching
+      if(inputString.toLowerCase() == aName.toLowerCase()) { return -1; }
+      if(inputString.toLowerCase()== bName.toLowerCase()) { return 1; } 
+
+      if( (inputString!=aName) && (inputString!=bName) ){
+        var aNameCount = aName.replace( /[^\w ]/g, "" ).split( /\s+/ ).length;
+        var bNameCount = bName.replace( /[^\w ]/g, "" ).split( /\s+/ ).length;
+        if (aNameCount < bNameCount) {
+          return -1;
+        } else if (aNameCount > bNameCount) {
+          return 1;
+        } else
+          return 0;
+      }
+    }
+  });
+
+  $('.typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  }, 
+  {
+    name: 'professions',
+    display: 'name',
+    source: professions,
+    limit: 10
+  });
+
+});
+
+
+// $(function() {
+//   var mydata;
+//   $.ajax({
+//     url: 'professions'
+//   }).done(function(data) {
+//     mydata = data;
+//     console.log(mydata);
+//   });
+// });
