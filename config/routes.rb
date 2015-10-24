4653e025f1cb5697abb6e67abec281bd4cffa778
antoniozdelican <antonio.zdelican@gmail.com>
2015-10-24 18:53:18 +0100

    put devise views into users views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   root 'home#index'
   
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords' }
   resources :users, only: [:show]
 
   resources :professions, only: [:index]
