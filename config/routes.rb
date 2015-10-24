62b8080c32ceed254acaec7685dbf81d4a438f22
antoniozdelican <antonio.zdelican@gmail.com>
2015-10-24 18:40:33 +0100

    added overrides for devise registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
   
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:show]
 
   resources :professions, only: [:index]
