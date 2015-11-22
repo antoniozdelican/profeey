e7cce027872dcda0f02dd8b2ad6ef9eda5ce1cb4
antoniozdelican <antonio.zdelican@gmail.com>
2015-11-22 20:40:28 +0000

    added jquery functionality for checking and unchecking following people

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -30,18 +30,62 @@ $(document).on('page:load', cropit);
 var checker;
 checker = function() {
   var checkbox = $('.the-checkbox');
+  var checkboxAll = $('.the-checkbox-all');
 
+  // checkbox for single person
   checkbox.on('click', function(e) {
     e.preventDefault();
     var checkboxContainer = $(this).closest('.the-checkbox-container');
+
+    // uncheck
     if ($(this).hasClass('the-check-person')) {
       $(this).addClass('hidden');
       checkboxContainer.find('.the-uncheck-person').removeClass('hidden');
       checkboxContainer.find('.the-checkbox-real').prop('checked', false);
-    } else {
+      // check if checkbox for all people is checked
+      // if yes, uncheck it
+      if ($('.the-checkbox-all-real').prop('checked')) {
+        $('.the-check-all-people').addClass('hidden');
+        $('.the-uncheck-all-people').removeClass('hidden');
+        $('.the-checkbox-all-real').prop('checked', false);
+      }
+    } 
+    // check
+    else {
       $(this).addClass('hidden');
       checkboxContainer.find('.the-check-person').removeClass('hidden');
-      checkboxContainer.find('.the-checkbox-real').prop('checked', true);
+      checkboxContainer.find('.the-checkbox-real').prop('checked', true);    
+      // check if all single checkboxes are checked
+      // if yes, check checkbox for all people
+      if ($('.the-checkbox-real:checked').length == $('.the-checkbox-real').length) {
+        console.log('yes');
+        $('.the-uncheck-all-people').addClass('hidden');
+        $('.the-check-all-people').removeClass('hidden');
+        $('.the-checkbox-all-real').prop('checked', true);
+      }
+    }
+  });
+
+  // checkbox for all people
+  checkboxAll.on('click', function(e) {
+    e.preventDefault();
+    var checkboxContainer = $(this).closest('.the-checkbox-container');
+    if ($(this).hasClass('the-check-all-people')) {
+      $(this).addClass('hidden');
+      checkboxContainer.find('.the-uncheck-all-people').removeClass('hidden');
+      checkboxContainer.find('.the-checkbox-all-real').prop('checked', false);
+      // uncheck all people
+      $('.the-check-person').addClass('hidden');
+      $('.the-uncheck-person').removeClass('hidden');
+      $('.the-checkbox-real').prop('checked', false);
+    } else {
+      $(this).addClass('hidden');
+      checkboxContainer.find('.the-check-all-people').removeClass('hidden');
+      checkboxContainer.find('.the-checkbox-all-real').prop('checked', true);
+      // check all people
+      $('.the-uncheck-person').addClass('hidden');
+      $('.the-check-person').removeClass('hidden');
+      $('.the-checkbox-real').prop('checked', true);
     }
   });
 }
