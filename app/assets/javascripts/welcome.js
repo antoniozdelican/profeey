a1af0e40cfe9a04f77352762bcf869921e930535
antoniozdelican <antonio.zdelican@gmail.com>
2015-11-22 19:27:34 +0000

    added people to follow and mimic for the checkboxes

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,7 +1,7 @@
 // Welcome flow JavaScript
 
+// detects crop positions for Paperclip custom cropper processor
 var cropit;
-
 cropit = function() {
   var profilePicUpload = $('.the-profile-pic-upload');
   
@@ -25,3 +25,26 @@ cropit = function() {
 
 $(document).ready(cropit);
 $(document).on('page:load', cropit);
+
+// mimics checkbox for custom checkboxes to select following people
+var checker;
+checker = function() {
+  var checkbox = $('.the-checkbox');
+
+  checkbox.on('click', function(e) {
+    e.preventDefault();
+    var checkboxContainer = $(this).closest('.the-checkbox-container');
+    if ($(this).hasClass('the-check-person')) {
+      $(this).addClass('hidden');
+      checkboxContainer.find('.the-uncheck-person').removeClass('hidden');
+      checkboxContainer.find('.the-checkbox-real').prop('checked', false);
+    } else {
+      $(this).addClass('hidden');
+      checkboxContainer.find('.the-check-person').removeClass('hidden');
+      checkboxContainer.find('.the-checkbox-real').prop('checked', true);
+    }
+  });
+}
+
+$(document).ready(checker);
+$(document).on('page:load', checker);
