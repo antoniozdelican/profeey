a171d2a4dd844f32d14f16762aa7c2fbca5f2d60
antoniozdelican <antonio.zdelican@gmail.com>
2015-11-23 14:17:06 +0000

    added following integration test and some follor_relationships fixtures

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,19 +1,24 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  # Adding Devise helper
   include Devise::TestHelpers
+  
   def setup
     @user = users(:antonio)
     @other_user = users(:ivan)
   end
+
   test "should redirect index when not logged in" do
       get :index
       assert_redirected_to new_user_session_path
     end
+
   test "should redirect followers when not logged in" do
     get :followers, id: @user
     assert_redirected_to new_user_session_path
   end
+  
   test "should redirect following when not logged in" do
     get :following, id: @user
     assert_redirected_to new_user_session_path
