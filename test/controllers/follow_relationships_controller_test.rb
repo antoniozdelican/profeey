1981caa68895a50ae3d529d08c86e1f64c363e40
antoniozdelican <antonio.zdelican@gmail.com>
2015-11-23 14:48:13 +0000

    added actions to follow_relationships controller and tests

--- test/controllers/follow_relationships_controller_test.rb
+++ test/controllers/follow_relationships_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class FollowRelationshipsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Adding Devise helper
+  include Devise::TestHelpers
+  
+  test "create should require logged-in user" do
+    assert_no_difference 'FollowRelationship.count' do
+      post :create
+    end
+    assert_redirected_to new_user_session_path
+  end
+
+  test "destroy should require logged-in user" do
+    assert_no_difference 'FollowRelationship.count' do
+      delete :destroy, id: follow_relationships(:one)
+    end
+    assert_redirected_to new_user_session_path
+  end
 end
